4020760dccc5331da91f4fc4d8e1dc0cb1daef56
Daniel Levi <daniellevi000@gmail.com>
2024-02-25 19:39:23 -0300

    banco de daods h2 e criacao de banco categoria automatica

--- src/main/java/com/example/UMl/Domain/Categoria.java
+++ src/main/java/com/example/UMl/Domain/Categoria.java
@@ -2,12 +2,24 @@ package com.example.UMl.Domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
 public class Categoria implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+  private String nome;
+
   public Categoria(Integer id) {
     this.id = id;
   }
-  private String nome;
 
   public Categoria() {
 
