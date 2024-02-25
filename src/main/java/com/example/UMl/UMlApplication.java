63ccd4f5868e003a3178a6671a4c12a1f2be1301
Daniel Levi <daniellevi000@gmail.com>
2024-02-25 18:46:28 -0300

    create api

---
+++ src/main/java/com/example/UMl/UMlApplication.java
@@ -0,0 +1,13 @@
+package com.example.UMl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UMlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UMl/UMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UMl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
